04accb971503c1be18c2a0f17582763c6b652a0f
MaxVeltwisch <45353558+MaxVeltwisch@users.noreply.github.com>
2019-05-27 13:03:35 +0200

    Update LSTM_Model.py

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -22,3 +22,88 @@ TecDAX_data["Date"] = pd.to_datetime(TecDAX_data["Date"])      # convert 'Date' 
 TecDAX_data.set_index("Date", inplace=True)                  # set index       
 TecDAX_data.info()
 TecDAX_data.sort_index(inplace=True)                         # sort index
+
+# extract adjusted closing prices (variable to predict)
+TecDAX_close = TecDAX_data["Adj Close"]
+TecDAX_close = TecDAX_close.values.reshape(len(TecDAX_close),1)
+
+# split data into train and test data
+train_size = int(len(TecDAX_close) * 0.7)
+test_size  = len(TecDAX_close) - train_size
+
+TecDAX_train, TecDAX_test = TecDAX_close[0:train_size,:], TecDAX_close[train_size:len(TecDAX_close),:]
+
+print("Training observations: ", len(TecDAX_train))
+print("Testing observations:  ", len(TecDAX_test))
+
+# normalize data
+from sklearn.preprocessing import MinMaxScaler
+
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(TecDAX_train)
+test_set_scaled     = sc.fit_transform(TecDAX_train)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding a second LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a third LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
+
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((pd.Series(TecDAX_train[:,0]), pd.Series(TecDAX_test[:,0])), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(TecDAX_test) - 60:]
+inputs = inputs.values.reshape(-1,1)
+inputs = sc.transform(inputs)
+X_test = []
+
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+    
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(TecDAX_test[0:20,:], color = 'red', label = 'Real TecDAX')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted TecDAX')
+plt.title('TecDAX Prediction')
+plt.xlabel('Time')
+plt.ylabel('Index Price')
+plt.legend()
+plt.show()
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
